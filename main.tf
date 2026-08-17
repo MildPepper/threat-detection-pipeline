3ec5abbd64bc65ee7fa151e2bc7a23cc6a167471
MildPepper <mildpepper25@gmail.com>
2026-08-17 21:57:16 +0530

    Fix terraform formatting_2

--- main.tf
+++ main.tf
@@ -118,3 +118,31 @@ resource "aws_lambda_function" "threat_evaluator" {
   filename         = "lambda_function.zip"
   source_code_hash = filebase64sha256("lambda_function.zip")
 }
+
+
+resource "aws_s3_bucket_public_access_block" "raw_events" {
+  bucket = aws_s3_bucket.raw_events.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "raw_events" {
+  bucket = aws_s3_bucket.raw_events.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "raw_events" {
+  bucket = aws_s3_bucket.raw_events.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
